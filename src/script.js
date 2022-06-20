ce0af12f8b18afe9ee1dbb94b08bfb3df6218426
Farruh <farruh020202@gmail.com>
2022-06-20 21:33:22 +0500

    feat: add styles and html

--- src/script.js
+++ src/script.js
@@ -1,6 +1,36 @@
-window.addEventListener('load', function () {
-    const date = new Date().toLocaleDateString() + ', ' + new Date().toLocaleTimeString();
+window.addEventListener("load", function () {
+  const date =
+    new Date().toLocaleDateString() + ", " + new Date().toLocaleTimeString();
+  const weekDay = new Date().getDay();
 
-    const dateHtml = document.querySelector('.header-date');
-    dateHtml.innerHTML = date;
-})
+  const dateHtml = document.querySelector(".header-date");
+  dateHtml.innerHTML = date;
+
+  const weekDayHtml = document.querySelector(".header-day");
+
+  switch (weekDay) {
+    case 0:
+      weekDayHtml.innerHTML = "Воскресенье: 09:00 - 20:00";
+      break;
+    case 1:
+      weekDayHtml.innerHTML = "Понедельник: 09:00 - 20:00";
+      break;
+    case 2:
+      weekDayHtml.innerHTML = "Вторник: 09:00 - 20:00";
+      break;
+    case 3:
+      weekDayHtml.innerHTML = "Среда: 09:00 - 20:00";
+      break;
+    case 4:
+      weekDayHtml.innerHTML = "Четверг: 09:00 - 20:00";
+      break;
+    case 5:
+      weekDayHtml.innerHTML = "Пятница: 09:00 - 20:00";
+      break;
+    case 6:
+      weekDayHtml.innerHTML = "Суббота: 09:00 - 20:00";
+      break;
+    default:
+      break;
+  }
+});
